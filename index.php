c1352687db6748a69b0a604695248f99214a2736
Eerik <eerik.lehtomaki@gmail.com>
2019-03-21 20:52:16 +0200

    asd

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo file_get_contents("header.html"); 
         
 echo '
-<h1>Just a landing page.</h1>
+<p>Just a landing page.<p>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/veqM5_ReprM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 ';
 
